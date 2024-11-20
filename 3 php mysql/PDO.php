e239e3007765063d9bff14e41ea3fc3434c49cb5
J Melg <rjmzelaya@gmail.com>
2024-11-20 12:36:05 -0600

    PHP connection to MySQL Server

---
+++ 3 php mysql/PDO.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPHP</title>
+</head>
+<body>
+    <p>
+        <?php
+        //new PDO('mysql:host=hostname;dbname=database', 'username', 'password');
+        //echo 'mysql:host='.$myHost.';dbname=ibdb', $myUser, $myPass;
+        $myUser = $_POST['myUser'];
+        $myHost = $_POST['myHost'];
+        $myPass = $_POST['myPassword'];
+
+        try{
+            $pdo =  new PDO('mysql:host='.$myHost.';dbname=ibdb;charset=utf8md4', $myUser, $myPass);
+            $output = 'Connection established successfully B)';
+        }
+        catch(PDOException $e){
+            $output = 'Unable to connect to db server '.$e->getMessage(). 'in ->>>> '.$e->getFile(). ' :'.$e->getLine();
+        }
+        
+        //output
+        echo $output;
+        ?>    
+    </p>
+    <a href="table.php"><button>Create table</button></a>
+</body>
+</html>
+    
---
+++ 3 php mysql/table.php
@@ -0,0 +1,25 @@
+<?php
+    
+    echo '<p> welcome</p>';
+
+    //lets create a table
+    $query = 'CREATE TABLE Tracks (
+        id int NOT NULL PRIMARY KEY,
+        `name` nchar(250) NOT NULL,
+        `length` time,
+        album int,
+        FOREIGN KEY (album) REFERENCES Albums(id),
+        `artist` int,
+        FOREIGN KEY (artist) REFERENCES Artists(id)
+    );';
+
+    //try to do it
+    try {
+        $pdo->exec($query);
+        $output = 'Great! the table Tracks was created succesfully!';
+    }
+    catch(PDOException $e) {
+        $output = 'Something went wrong ....'.$e->getMessage(). 'in ->>>> '.$e->getFile(). ' :'.$e->getLine();
+    }
+    echo '<p>'.$output.'</p>';
+    $pdo = null; //close the connection
